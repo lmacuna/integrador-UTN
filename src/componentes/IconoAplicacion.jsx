a0b6b4e679d3c83128f0f6d3a3f1eb9a38712778
lucas <lucasmacuna@gmail.com>
2023-11-25 20:52:36 -0300

    PATH imagen

--- src/componentes/IconoAplicacion.jsx
+++ src/componentes/IconoAplicacion.jsx
@@ -5,7 +5,7 @@ import '../hojas-de-estilo/IconoAplicacion.css'
 const IconoAplicacion = () => {
     return ( 
         <div style={{display:'flex',justifyContent:'start',alignItems:'center'}}>
-          <img className='icono-aplicacion' src='src\imagenes\utn.png' alt='icono de la aplicacion'></img> <h2>Argentina programa 4.0</h2>
+          <img className='icono-aplicacion' src='utn.png' alt='icono de la aplicacion'></img> <h2>Argentina programa 4.0</h2>
         </div>
      );
 }
